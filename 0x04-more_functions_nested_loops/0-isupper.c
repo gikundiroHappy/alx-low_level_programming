7ecdc2aca02abe73dbdef606477eabebe2ad7e51
root <user.email>
2022-03-21 00:45:36 -0700

    Upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: character to be checked
+ *
+ * Return: 1 if it is upper, otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
