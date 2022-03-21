8a65a799a209bf83387687464fd4cc2bf07ca02b
root <user.email>
2022-03-21 01:06:33 -0700

    Product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - add two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return:value
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
