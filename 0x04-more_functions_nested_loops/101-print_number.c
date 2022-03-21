5bf1049b16f199bf4ca61d699fbe61b23a2c8d87
root <user.email>
2022-03-21 05:54:40 -0700

    Print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: number to be printed
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n >= 0)
+	{
+		if (n >= 10)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		-putchar('-');
+		m = -1 * n;
+		if (m >= 10)
+			print_number(m / 10);
+		_putchar('0' + m % 10);
+	}
+}
