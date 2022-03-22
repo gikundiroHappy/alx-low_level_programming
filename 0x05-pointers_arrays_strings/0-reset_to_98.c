ba0009f8215d27025f0013e80111d7630b0e74f9
root <user.email>
2022-03-22 00:01:45 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - print the value of pointer
+ * @n: pointer of n and return value of 98
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
