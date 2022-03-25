88b1ff370b8fef1a77adc080e4b18396d6cf1b3d
root <user.email>
2022-03-24 21:26:21 -0700

    string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two string
+ * @s1: the first string
+ * @s2: the second string
+ * Return: value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0, k = 0, r = 0, lim;
+
+	while (s1[i])
+	{
+		i++;
+	}
+	while (s2[j])
+	{
+		j++;
+	}
+	if (i <= j)
+	{
+		lim = i;
+	}
+	else
+	{
+		lim = j;
+	}
+	while (k <= lim)
+	{
+		if (s1[k] == s2[k])
+		{
+			k++;
+			continue;
+		}
+		else
+		{
+			r = s1[k] - s2[k];
+			break;
+		}
+		k++;
+	}
+	return (r);
+}
