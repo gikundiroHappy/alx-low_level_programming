98360994ae5607450de8c3aa96f79968dff5fb0b
root <user.email>
2022-04-05 00:59:19 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to newly allocate space in memory
+ * @str: array
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *string;
+
+	if (str == NULL)
+		return (0);
+
+	i = 0;
+
+	while (*(str + i) != '\0')
+		i++;
+
+	string = malloc(i + 1);
+	if (string == 0)
+		return (0);
+
+	for (j = 0; j < i; j++)
+		*(string + j) = *(str + j);
+
+	return (string);
+}
