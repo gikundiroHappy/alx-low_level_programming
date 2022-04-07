39808ceb03a6b064074567ac89a3c6e7d1f544d8
root <user.email>
2022-04-07 11:44:49 -0700

    malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,9 @@
 #include "main.h"
 
 /**
- * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- *
- * Return: a pointer to the allocated memory
+ * malloc_checked - allocates memory using malloc
+ * @b: size to be allocate
+ * Return: pointer to  allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
