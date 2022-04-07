123031d9b15f794dc07380fdb3763297567e4913
root <user.email>
2022-04-07 11:48:50 -0700

    malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,13 +8,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *a;
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
+	a = malloc(b);
+	if (a == NULL)
 	{
 		exit(98);
 	}
-	return (ptr);
+	return (a);
 }
