41565cae0885868958e0efd19f17d9784c49cf16
root <user.email>
2022-04-26 14:57:51 -0700

    print listint safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,21 +9,18 @@
 size_t print_listint_safe(const listint_t *head)
 {
 		size_t num = 0;
-		long int diff;
 
-		while (head)
+		while (head && (head > head->next))
 		{
-			diff = head - head->next;
-			num++;
 			printf("[%p] %d\n", (void *)head, head->n);
-			if (diff > 0)
-				head = head->next;
-			else
-			{
-				printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-				break;
-			}
+			head = head->next;
+			++num;
 		}
-
-		return (num);
+			if (head)
+			{
+				printf("[%p] %d\n", (void *)head, head->n);
+				printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+				++num;
+			}
+			return (num);
 }
